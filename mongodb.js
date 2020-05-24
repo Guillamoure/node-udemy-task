8dfe12bc03b38f908a2e3538c70167ce8d35b207
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-24 15:15:59 -0400

    lesson 80: updating documents

--- mongodb.js
+++ mongodb.js
@@ -23,33 +23,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
       console.log(result.ops)
     })
   }
-  //
-  // db.collection('users').findOne({ _id: new ObjectID("5ec96828bd7a8701b607cdb0") }, (error, user) => {
-  //   if (error) {return console.log("Unable to fetch")}
-  //   console.log(user)
-  // })
-  //
-  // db.collection('users').find({ age: 21 }).toArray((error, user) => {
-  //   if (error) {return console.log("Unable to fetch")}
-  //   console.log(user)
-  // })
-  //
-  // db.collection('users').find({ age: 21 }).count((error, count) => {
-  //   if (error) {return console.log("Unable to fetch")}
-  //   console.log(count)
-  // })
 
-  db.collection('tasks').findOne({ _id: new ObjectID("5ec969878986d7e85de17125")}, (error, task) => {
-    console.log(task)
+  db.collection('tasks').updateMany({
+    completed: false
+  }, {
+    $set: { completed: true }
   })
-
-  db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-    console.log(tasks)
-  })
-  // mongodbInsertMany('tasks', [
-  //   { description: "Cooked Lunch", completed: true},
-  //   { description: "Cleaned House", completed: false},
-  //   { description: "Mailed Letter", completed: false}
-  // ])
+    .then((result) => {
+      console.log(result)
+    }).catch((error) => {
+      console.log(error)
+    })
 
 })
