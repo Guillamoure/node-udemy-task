e952872a07ed828422bc9a37ab7ad9fcecb255a6
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-24 14:33:19 -0400

    lesson 78: querying documents

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,5 @@
 // CRUD
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -24,11 +23,33 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
       console.log(result.ops)
     })
   }
+  //
+  // db.collection('users').findOne({ _id: new ObjectID("5ec96828bd7a8701b607cdb0") }, (error, user) => {
+  //   if (error) {return console.log("Unable to fetch")}
+  //   console.log(user)
+  // })
+  //
+  // db.collection('users').find({ age: 21 }).toArray((error, user) => {
+  //   if (error) {return console.log("Unable to fetch")}
+  //   console.log(user)
+  // })
+  //
+  // db.collection('users').find({ age: 21 }).count((error, count) => {
+  //   if (error) {return console.log("Unable to fetch")}
+  //   console.log(count)
+  // })
 
-  mongodbInsertMany('tasks', [
-    { description: "Cooked Lunch", completed: true},
-    { description: "Cleaned House", completed: false},
-    { description: "Mailed Letter", completed: false}
-  ])
+  db.collection('tasks').findOne({ _id: new ObjectID("5ec969878986d7e85de17125")}, (error, task) => {
+    console.log(task)
+  })
+
+  db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+    console.log(tasks)
+  })
+  // mongodbInsertMany('tasks', [
+  //   { description: "Cooked Lunch", completed: true},
+  //   { description: "Cleaned House", completed: false},
+  //   { description: "Mailed Letter", completed: false}
+  // ])
 
 })
