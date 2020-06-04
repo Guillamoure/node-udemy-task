290f14dcdb998e960034b74845483bc84b3e8655
Jack Hilscher <jhilscheriv@gmail.com>
2020-06-04 16:06:08 -0400

    lesson 93: promise chaining

---
+++ playground/promise-chaining.js
@@ -0,0 +1,14 @@
+require('../src/db/mongoose')
+const User = require('../src/models/user')
+
+// 5ecd617f3a85a37766ce4315
+
+User.findByIdAndUpdate('5ecd617f3a85a37766ce4315', { age: 1 })
+  .then(user => {
+    console.log(user)
+    return User.countDocuments({ age: 1 })
+  })
+  .then(result => {
+    console.log(result)
+  })
+  .catch(console.log)
