6eaaef4cbae1951fb00596ad9fa8ab6c0136cd00
Jack Hilscher <jhilscheriv@gmail.com>
2020-08-15 10:46:41 -0400

    lesson 116: dependent destroy

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -94,6 +95,15 @@ userSchema.pre('save', async function(next) {
   }
 
   next()
+})
+
+// DELETE USER TASKS WHEN USER IS REMOVED
+userSchema.pre('remove', async function(next) {
+	const user = this
+
+	await Task.deleteMany({ author: user._id })
+
+	next()
 })
 
 const User = mongoose.model('User', userSchema)
