a96abadd2a1f8c271bbbc85f1bab4e2c687ed94d
Jack Hilscher <jhilscheriv@gmail.com>
2020-08-15 16:28:38 -0400

    lesson 126: handle errors express

--- src/index.js
+++ src/index.js
@@ -10,8 +10,14 @@ const multer = require('multer')
 const upload = multer({
 	dest: 'images'
 })
+
+const errorMiddleware = (req, res, next) => {
+	throw new Error('From my middleware')
+}
 app.post('/upload', upload.single('upload'), (req, res) => {
 	res.send()
+}, (error, req, res, next) => {
+	res.status(400).send({error: error.message})
 })
 
 
