d37fbf163003e538088075a6d8e67ede888be76e
Jack Hilscher <jhilscheriv@gmail.com>
2020-08-15 10:42:07 -0400

    lesson 115: additional auth practice

--- src/routers/task.js
+++ src/routers/task.js
@@ -16,19 +16,19 @@ router.post('/tasks', auth, async (req, res) => {
   }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
   try {
-    const tasks = await Task.find({ })
-    res.send(tasks)
+		await req.user.populate('tasks').execPopulate()
+    res.send(req.user.tasks)
   } catch (e) {
     res.status(500).send()
   }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
   const _id = req.params.id
   try {
-    const task = await Task.findById(_id)
+		const task = await Task.findOne({ _id, author: req.user._id })
     if (!task){return res.status(404).send()}
     res.send(task)
   } catch (e) {
@@ -36,7 +36,7 @@ router.get('/tasks/:id', async (req, res) => {
   }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['description', 'completed']
   const isValidOperation = updates.every(update => allowedUpdates.includes(update))
@@ -45,21 +45,23 @@ router.patch('/tasks/:id', async (req, res) => {
 
   const _id = req.params.id
   try {
-    const task = await Task.findById(_id)
-    updates.forEach(update => task[update] = req.body[update])
-    await task.save()
+    const task = await Task.findOne({ _id, author: req.user._id})
 
     if(!task){return res.status(404).send()}
-    res.send(task)
+
+		updates.forEach(update => task[update] = req.body[update])
+    await task.save()
+
+		res.send(task)
   } catch (e) {
     res.status(500).send(e)
   }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
   const _id = req.params.id
   try {
-    const task = await Task.findByIdAndDelete(_id)
+    const task = await Task.findOneAndDelete({ _id, author: req.user._id })
     if (!task){return res.status(404).send()}
     res.send(task)
   } catch (e) {
