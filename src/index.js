e1f466d13d59fc5cb6fed2188f651389c3249115
Jack Hilscher <jhilscheriv@gmail.com>
2020-08-15 16:58:26 -0400

    lesson 129: reformating images with sharp

--- src/index.js
+++ src/index.js
@@ -6,22 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 4000
 
-const multer = require('multer')
-const upload = multer({
-	dest: 'images'
-})
-
-const errorMiddleware = (req, res, next) => {
-	throw new Error('From my middleware')
-}
-app.post('/upload', upload.single('upload'), (req, res) => {
-	res.send()
-}, (error, req, res, next) => {
-	res.status(400).send({error: error.message})
-})
-
-
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
