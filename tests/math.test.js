3023766978d9fb60a01f9ff663dfbd654a4018b5
Jack Hilscher <jhilscheriv@gmail.com>
2020-08-16 21:02:16 -0400

    lesson 139: practicing tests

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip } = require('../src/math')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
 
 test('Should calculate total with tip', () => {
 	const total = calculateTip(10, .3)
@@ -8,4 +8,14 @@ test('Should calculate total with tip', () => {
 
 test('Should calculate total with default tip', () => {
 	expect(calculateTip(10)).toBe(12)
+})
+
+test('Should convert 32 F to 0 C', () => {
+	expect(fahrenheitToCelsius(32)).toBe(0)
+	expect(fahrenheitToCelsius(-40)).toBe(-40)
+})
+
+test('Should convert 0 C to 32 F', () => {
+	expect(celsiusToFahrenheit(0)).toBe(32)
+	expect(celsiusToFahrenheit(-40)).toBe(-40)
 })
