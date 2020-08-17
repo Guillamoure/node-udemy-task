b68c4c7422d6d3d5683087966815eccd2d946d3c
Jack Hilscher <jhilscheriv@gmail.com>
2020-08-16 20:55:34 -0400

    lesson 138: practicing jest

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,11 @@
-test('Hello World', () => {
+const { calculateTip } = require('../src/math')
 
+test('Should calculate total with tip', () => {
+	const total = calculateTip(10, .3)
+	expect(total).toBe(13)
+	expect(calculateTip(100, .2)).toBe(120)
 })
 
- test('This should fail', () => {
-	 throw new Error('Failure')
- })
+test('Should calculate total with default tip', () => {
+	expect(calculateTip(10)).toBe(12)
+})
