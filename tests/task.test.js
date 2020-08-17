7118f064c4a4e6aaea833bec5d96ee40ca59b4f5
Jack Hilscher <jhilscheriv@gmail.com>
2020-08-17 18:29:17 -0400

    lesson 149: additional jest testing

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const app = require('../src/app')
 const Task = require('../src/models/task')
-const { userOneId, userOne, configureDatabase } = require('./fixtures/db')
+const { userOneId, userOne, configureDatabase, userTwo, taskTwo } = require('./fixtures/db')
 
 beforeEach(configureDatabase)
 
@@ -17,4 +17,24 @@ test('Should create task for user', async () => {
 	const task = await Task.findById(response.body._id)
 	expect(task).not.toBeNull()
 	expect(task.completed).toBe(false)
+})
+
+test('Should access all tasks for one user', async () => {
+	var response = await request(app)
+		.get('/tasks')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.expect(200)
+
+		expect(response.body.length).toBe(2)
+})
+
+test('Should not be able to delete other users task', async () => {
+	const response = await request(app)
+		.delete(`/tasks/${taskTwo._id}`)
+		.set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+		.send()
+		.expect(404)
+
+		const task = await Task.findById(taskTwo._id)
+		expect(task).not.toBeNull()
 })
