76f3acdf5c73287c1a70122c2fab4dd15266a553
Jack Hilscher <jhilscheriv@gmail.com>
2020-08-16 21:22:35 -0400

    lesson 140: async test

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math')
 
 test('Should calculate total with tip', () => {
 	const total = calculateTip(10, .3)
@@ -18,4 +18,23 @@ test('Should convert 32 F to 0 C', () => {
 test('Should convert 0 C to 32 F', () => {
 	expect(celsiusToFahrenheit(0)).toBe(32)
 	expect(celsiusToFahrenheit(-40)).toBe(-40)
+})
+
+// test('Async', (done) => {
+// 	setTimeout(() => {
+// 		expect(1).toBe(2)
+// 		done()
+// 	}, 2000)
+// })
+
+test('Should add two numbers', (done) => {
+	add(2, 3).then((sum) => {
+		expect(sum).toBe(5)
+		done()
+	})
+})
+
+test('Should add two numbers async/await', async () => {
+	const sum = await add(2, 5)
+	expect(sum).toBe(7)
 })
