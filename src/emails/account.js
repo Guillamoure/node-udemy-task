e45fd6aa59be35ff2ab982496008b05a88799eb1
Jack Hilscher <jhilscheriv@gmail.com>
2020-08-16 22:58:14 -0400

    lesson143: jest setup and more express tests

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,15 +1,15 @@
 const sgMail = require('@sendgrid/mail')
 require('dotenv').config()
 
-
-console.log(process.env.SENDGRID_API_KEY)
+//
+// console.log(process.env.SENDGRID_API_KEY)
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
 	sgMail.send({
 		to: email,
-		from: '',
+		from: `${process.env.EMAIL}`,
 		subject: 'Welcome to the App!',
 		text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
 	})
@@ -18,7 +18,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelEmail = (email, name) => {
 	sgMail.send({
 		to: email,
-		from: '',
+		from: `${process.env.EMAIL}`,
 		subject: 'Sorry to see you go!',
 		text: `Sorry to see you go, ${name}. Let us know if there was anything we could've done to keep you with us!`
 	})
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+	name: 'Mike',
+	email: 'mike4@example.com',
+	password: '56what!!'
+}
+
+beforeEach(async () => {
+	await User.deleteMany()
+	await new User(userOne).save()
+})
 
  test('Should signup a new user', async () => {
 	 await request(app).post('/users').send({
@@ -8,3 +20,17 @@ const app = require('../src/app')
 		 password: 'MyPassword777!'
 	 }).expect(201)
  })
+
+test('Should login existing user', async () => {
+	await request(app).post('/users/login').send({
+		email: userOne.email,
+		password: userOne.password
+	}).expect(200)
+})
+
+test('Should not login nonexistant user', async () => {
+	await request(app).post('/users/login').send({
+		email: "nahnah@example.com",
+		password: "doesNotExist8?"
+	}).expect(400)
+})
